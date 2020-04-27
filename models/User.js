18e6a9b6694b7d658726350d992dbc609d895f87
Anne Patana <apatana@hotmail.com>
2020-04-27 15:20:39 +0200

    models done

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  email: {type: String, required: true},
+  password: {type: String, required: true},
+  dateOfBirth: Date,
+  address: {
+      street: String,
+    city: String
+},
+description: String,
+profilePicture: {type: String, default: "./images/default_user.PNG" },
+restrictions: { type: Schema.Types.ObjectId, ref: "Tag" }
+
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
