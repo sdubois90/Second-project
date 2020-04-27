5e152cfdb2a98eafeaf8d32a839ddd7271989c4f
Anne Patana <apatana@hotmail.com>
2020-04-27 16:03:58 +0200

    server setup

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+require("./config/dbconnect");
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -15,20 +18,23 @@ app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
