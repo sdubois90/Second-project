30b0c662a9727d480cc3c9e3e867ddfe14973826
Nelly BORJON-PIRON <nelly.borjon@hotmail.fr>
2020-04-28 17:00:28 +0200

    edit profile almost done

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,29 +1,29 @@
-  require("dotenv").config();
+require("dotenv").config();
+
   const mongoose = require("mongoose");
   // const User = require("../models/User");
-  const Event = require("../models/Event");
-  // const Tag = require("../models/Tag");
+  // const Event = require("../models/Event");
+  const Tag = require("../models/Tag");
 
-  // const tags = [
-  //   {
-  //     restrictions: "Vegetarian"
-  //   },
-  //   {
-  //     restrictions: "Vegan"
-  //   },
-  //   {
-  //     restrictions: "Gluten free"
-  //   },
-  //   {
-  //     restrictions: "Halal"
-  //   },
-  //   {
-  //     restrictions: "Egg free"
-  //   },
-  //   {
-  //     restrictions: "Meat only"
-  //   },
-  // ];
+  const tags = [{
+      restrictions: "Vegetarian"
+    },
+    {
+      restrictions: "Vegan"
+    },
+    {
+      restrictions: "Gluten free"
+    },
+    {
+      restrictions: "Halal"
+    },
+    {
+      restrictions: "Egg free"
+    },
+    {
+      restrictions: "Meat only"
+    }
+  ];
 
   // const users = [
   //   {
@@ -125,137 +125,135 @@
   //   },
   // ];
 
-  const events = [
-    {
-    eventName: "Dinner with Nelly",
+  //   const events = [
+  //     {
+  //     eventName: "Dinner with Nelly",
 
-    // chef: {
-    //   type: Schema.Types.ObjectId,
-    //   ref: "User"
-    // },
-    menu: {
-      starter: "Velouté de champignons",
-      main: "Risotto au safran de Sardaigne, légumes de saison",
-      dessert: "La crème brûlée",
-    },
-    type: "dinner",
-    //   dateOfEvent: Date,
-    location: {
-      street: "Sreet Name 1",
-      city: "Paris"
-    },
-    maxPeople: 5,
-    pricePP: 50,
-    theme: "Dinner in the dark",
+  //     // chef: {
+  //     //   type: Schema.Types.ObjectId,
+  //     //   ref: "User"
+  //     // },
+  //     menu: {
+  //       starter: "Velouté de champignons",
+  //       main: "Risotto au safran de Sardaigne, légumes de saison",
+  //       dessert: "La crème brûlée",
+  //     },
+  //     type: "dinner",
+  //     //   dateOfEvent: Date,
+  //     location: {
+  //       street: "Sreet Name 1",
+  //       city: "Paris"
+  //     },
+  //     maxPeople: 5,
+  //     pricePP: 50,
+  //     theme: "Dinner in the dark",
 
-    imgName: "Nelly the Chef",
-    imgPath: "./images/default_event.PNG",
+  //     imgName: "Nelly the Chef",
+  //     imgPath: "./images/default_event.PNG",
 
-    // restrictions: ["Vegetarian", "Gluten free"],
-    //   guests: {
-    //     type: Schema.Types.ObjectId,
-    //     ref: "User"
-  }, 
-  {
-  eventName: "Brunch with Nelly",
+  //     // restrictions: ["Vegetarian", "Gluten free"],
+  //     //   guests: {
+  //     //     type: Schema.Types.ObjectId,
+  //     //     ref: "User"
+  //   }, 
+  //   {
+  //   eventName: "Brunch with Nelly",
 
-    // chef: {
-    //   type: Schema.Types.ObjectId,
-    //   ref: "User"
-    // },
-    menu: {
-      starter: "---",
-      main: "Blueberry plush pancakes, warm maple syrup, chocolate croissants, espresso shots, strawberry/basil waffles, eggs, pomegranate muffins, watermelon",
-      dessert: "---",
-    },
-    type: "brunch",
-      // dateOfEvent: Date,
-    location: {
-      street: "Sreet Name 1",
-      city: "Paris"
-    },
-    maxPeople: 5,
-    pricePP: 50,
-    theme: "Brunch for the hangovers",
+  //     // chef: {
+  //     //   type: Schema.Types.ObjectId,
+  //     //   ref: "User"
+  //     // },
+  //     menu: {
+  //       starter: "---",
+  //       main: "Blueberry plush pancakes, warm maple syrup, chocolate croissants, espresso shots, strawberry/basil waffles, eggs, pomegranate muffins, watermelon",
+  //       dessert: "---",
+  //     },
+  //     type: "brunch",
+  //       // dateOfEvent: Date,
+  //     location: {
+  //       street: "Sreet Name 1",
+  //       city: "Paris"
+  //     },
+  //     maxPeople: 5,
+  //     pricePP: 50,
+  //     theme: "Brunch for the hangovers",
 
-    imgName: "Nelly the Chef",
-    imgPath: "./images/default_event.PNG",
+  //     imgName: "Nelly the Chef",
+  //     imgPath: "./images/default_event.PNG",
 
-    // restrictions: ["Vegetarian", "Gluten free"],
-      // guests: {
-      //   type: Schema.Types.ObjectId,
-      //   ref: "User"
-  }, 
-  {
-  eventName: "Dinner with Sebastien",
-    // chef: {
-    //   type: Schema.Types.ObjectId,
-    //   ref: "User"
-    // },
-    menu: {
-      starter: "Tartine crue avocat, citron vert",
-      main: "Aiguillettes de seitan marinées",
-      dessert: "Dôme mangue sauvage, vanille de tahiti et ylang-ylang",
-    },
-    type: "dinner",
-      // dateOfEvent: Date,
-    location: {
-      street: "Sreet Name 2",
-      city: "Paris"
-    },
-    maxPeople: 5,
-    pricePP: 50,
-    theme: "Dinner dinner dinner",
+  //     // restrictions: ["Vegetarian", "Gluten free"],
+  //       // guests: {
+  //       //   type: Schema.Types.ObjectId,
+  //       //   ref: "User"
+  //   }, 
+  //   {
+  //   eventName: "Dinner with Sebastien",
+  //     // chef: {
+  //     //   type: Schema.Types.ObjectId,
+  //     //   ref: "User"
+  //     // },
+  //     menu: {
+  //       starter: "Tartine crue avocat, citron vert",
+  //       main: "Aiguillettes de seitan marinées",
+  //       dessert: "Dôme mangue sauvage, vanille de tahiti et ylang-ylang",
+  //     },
+  //     type: "dinner",
+  //       // dateOfEvent: Date,
+  //     location: {
+  //       street: "Sreet Name 2",
+  //       city: "Paris"
+  //     },
+  //     maxPeople: 5,
+  //     pricePP: 50,
+  //     theme: "Dinner dinner dinner",
 
-    imgName: "Sebastien le Chef",
-    imgPath: "./images/default_event.PNG",
+  //     imgName: "Sebastien le Chef",
+  //     imgPath: "./images/default_event.PNG",
 
-    // restrictions: ["Vegetarian", "Gluten free"],
-    //   guests: {
-    //     type: Schema.Types.ObjectId,
-    //     ref: "User"
-  }, 
-  {
-  eventName: "Lunch with Anne",
-    // chef: {
-    //   type: Schema.Types.ObjectId,
-    //   ref: "User"
-    // },
-    menu: {
-      starter: "Sashimi de saumon accompagné de ses pomme grenailles à la vapeur, sauce du chef",
-      main: "Noix de veau, sauce aux morilles",
-      dessert: "Tiramisu à la mangue du chef",
-    },
-    type: "lunch",
-    //   dateOfEvent: Date,
-    location: {
-      street: "Sreet Name 3",
-      city: "Paris"
-    },
-    maxPeople: 5,
-    pricePP: 50,
-    theme: "Luuunch",
+  //     // restrictions: ["Vegetarian", "Gluten free"],
+  //     //   guests: {
+  //     //     type: Schema.Types.ObjectId,
+  //     //     ref: "User"
+  //   }, 
+  //   {
+  //   eventName: "Lunch with Anne",
+  //     // chef: {
+  //     //   type: Schema.Types.ObjectId,
+  //     //   ref: "User"
+  //     // },
+  //     menu: {
+  //       starter: "Sashimi de saumon accompagné de ses pomme grenailles à la vapeur, sauce du chef",
+  //       main: "Noix de veau, sauce aux morilles",
+  //       dessert: "Tiramisu à la mangue du chef",
+  //     },
+  //     type: "lunch",
+  //     //   dateOfEvent: Date,
+  //     location: {
+  //       street: "Sreet Name 3",
+  //       city: "Paris"
+  //     },
+  //     maxPeople: 5,
+  //     pricePP: 50,
+  //     theme: "Luuunch",
 
-    imgName: "Anne the Chef",
-    imgPath: "./images/default_event.PNG",
+  //     imgName: "Anne the Chef",
+  //     imgPath: "./images/default_event.PNG",
 
-    // restrictions: ["Vegetarian", "Gluten free"],
-  //     guests: {
-  //       type: Schema.Types.ObjectId,
-  //       ref: "User"
-  // }, 
-  },
-];
+  //     // restrictions: ["Vegetarian", "Gluten free"],
+  //   //     guests: {
+  //   //       type: Schema.Types.ObjectId,
+  //   //       ref: "User"
+  //   // }, 
+  //   },
+  // ];
 
   mongoose
-    .connect("mongodb://localhost/perfectlyFood", {
-      useNewUrlParser: true
-    })
+    .connect(process.env.MONGO_URI)
     .then((self) => {
       console.log(`Connected to ${self.connection.name}`);
 
       // Seeds
-      Event.create(events)
+      Tag.create(tags)
         .then((dbResponse) => {
           console.log(dbResponse);
         })
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const User = require('../models/User')
 const Tag = require('../models/Tag')
+const upload = require("../config/cloudinary");
 
 // router.get('/myprofile', (req, res) => {
 //   console.log(req.session.currentUser)
@@ -12,7 +13,42 @@ const Tag = require('../models/Tag')
 // });
 
 router.get('/myprofile', (req, res) => {
-  res.render('edit_profile');
+  Tag.find({})
+    .then((dbresult) => {
+      res.render('edit_profile', {
+        tags: dbresult
+      });
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 });
+
+router.post('/myprofile', upload.single("profilePicturePath"),(req, res) => {
+  let modifiedUser = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    address: {
+      street: req.body.street,
+      city: req.body.city,
+    },
+    description: req.body.description,
+    restrictions: req.body.restrictions
+  };
+  if (req.file) {
+    console.log(req.file)
+    modifiedUser.profilePicturePath = req.file.secure_url;
+  }
+  console.log(modifiedUser)
+  User.findOneAndUpdate(req.params.id, modifiedUser, { new: true, useFindAndModify: true })
+    .then((dbresult) => {
+      req.session.currentUser=dbresult
+      res.redirect("/myprofile")
+    })
+    .catch((err) => {
+    console.log(err)
+  })
+})
 
 module.exports = router;
