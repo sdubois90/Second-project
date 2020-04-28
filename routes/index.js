9b79d6d326a28f6a6197ceda0aa791dda336bc8d
Nelly BORJON-PIRON <nelly.borjon@hotmail.fr>
2020-04-28 22:52:30 +0200

    tuesday night

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ const uploadCloud = require('../config/cloudinary.js');
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
-    title: 'Perfectly Food'
+    title: 'Perfectly Food',
+    styles: ["index.css"]
   });
 });
 
